2046b0b56ba7ef586bf0164d3c0b1861303b36e3
seriey1 <devichenskiy.serafim@gmailcom>
2023-02-02 18:05:35 +0300

    добавленна влажность и температуру

---
+++ scp/func.php
@@ -0,0 +1,40 @@
+<?php
+
+use LDAP\Result;
+
+    function curl_defualt($ch, $r){
+        curl_setopt($ch,CURLOPT_CONNECTTIMEOUT, 2);
+        curl_setopt($ch,CURLOPT_CONNECTTIMEOUT_MS, 1000);
+        curl_setopt($ch,CURLOPT_NOSIGNAL, 1);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER, 1);
+        if($r){
+            $result = curl_exec($ch);
+            curl_close($ch);
+            return $result;
+        }
+        else{curl_exec($ch);}
+        curl_close($ch);
+    }
+    function get_sensor_tw($id, $type){
+        $ret = "";
+        $ch = curl_init("https://dt.miet.ru/ppo_it/api/temp_hum/$id");
+        $result = curl_defualt($ch, true);
+        $result = json_decode($result, true);
+        switch ($type) {
+            case '0':
+                $ret = $result["temperature"];
+                break;
+            case '1':
+                $ret = $result["humidity"];
+                break;
+            
+            default:
+                
+                break;
+        }
+        return $ret;
+    }
+
+
+
+?>
--- scp/index.php
+++ scp/index.php
@@ -1,3 +1,9 @@
 <?php 
-    echo "ebal&";
+    include_once 'func.php';
+    for($i = 1; $i<=4; $i++){
+        $h = get_sensor_tw($i, 1);
+        $t = get_sensor_tw($i, 0);
+        echo '#'.$i.' t='.$t.' h='.$h. '<br>';    
+    };
+   
 ?>
